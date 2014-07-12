258c696272177e2e3b543d6494839994bae8bb4c
xdite <xuite.joke@gmail.com>
2014-07-12 21:58:19 +0800

    do preview work

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:show, :edit, :update, :destroy]
+
 
 #  before_action :login_required, :only => [:create, :new, :update, :edit, :destroy]
 
@@ -16,7 +16,16 @@ class JobsController < ApplicationController
 
   # GET /jobs/new
   def new
-    @job = Job.new
+    if params[:token].present?
+      @job = Job.where(:token => params[:token]).first
+
+      if @job.blank?
+        @job = Job.new
+      end
+
+    else
+      @job = Job.new
+    end
   end
 
   # GET /jobs/1/edit
@@ -25,7 +34,23 @@ class JobsController < ApplicationController
 
 
   def preview
-    @job = Job.new(job_params)
+
+    if params[:job][:token].present?
+      @job = Job.find_by_token(params[:job][:token])
+      if @job.update(job_params)
+        render :preview
+      else
+        render :new
+      end
+    else
+
+      @job = Job.new(job_params)
+
+      if !@job.save
+        render :new
+      end
+    end
+    
   end
 
   # POST /jobs
@@ -47,14 +72,11 @@ class JobsController < ApplicationController
   # PATCH/PUT /jobs/1
   # PATCH/PUT /jobs/1.json
   def update
-    respond_to do |format|
-      if @job.update(job_params)
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
-        format.json { render :show, status: :ok, location: @job }
-      else
-        format.html { render :edit }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    @job = Job.find_by_token(params[:id])
+    if @job.update(job_params)
+      render :preview
+    else
+      render :new
     end
   end
 
@@ -76,6 +98,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:title, :description, :location , :company_name, :category_id , :apply_instruction)
+      params.require(:job).permit(:title, :description, :location , :company_name, :category_id , :apply_instruction, :url, :email)
     end
-end
+  end
