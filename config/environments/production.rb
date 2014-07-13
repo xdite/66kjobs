a54f51fc8ca3dcdf72c4a31e0df80daa007c5a01
xdite <xuite.joke@gmail.com>
2014-07-13 17:25:00 +0800

    add production cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+
+  config.assets.cache_store = :dalli_store
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
