fd2f128727c91319b94f502af562404694f5e711
xdite <xuite.joke@gmail.com>
2014-07-13 16:43:04 +0800

    Add about

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+  def about
+    set_page_title "關於本站"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
 
   resources :categories
   
+  get "/pages/:action" , :controller => "pages"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
