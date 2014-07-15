b80b56552c97035aea0a16ec1fbc94e02c76e345
xdite <xuite.joke@gmail.com>
2014-07-15 13:20:01 +0800

    Add mailing_list_member

---
+++ app/models/mailing_list_member.rb
@@ -0,0 +1,68 @@
+class MailingListMember
+  include ActiveModel::Validations
+
+  validates_presence_of :name, :address
+  attr_accessor :id, :mailing_list_address, :address, :name, :subscribed 
+ 
+
+  def initialize(attributes = {})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @attributes = attributes
+  end
+ 
+  def read_attribute_for_validation(key)
+    instance_variable_get("@#{key}")
+  end
+ 
+  def to_key
+
+  end
+
+  def save
+
+    if self.valid?
+      mailgun.list_members(mailing_list_address).add(address,
+        :name => name,
+        :subscribed => subscribed,
+        :upsert => true
+      )
+      return true
+    end
+    return false
+  end
+
+  def subscribed?
+    subscribed == true || subscribed == "yes" 
+  end
+
+  def self.find(mailing_list_address, address)
+    begin
+      member = mailgun.list_members(mailing_list_address).find(address)["member"]
+    rescue => e
+      member = {}
+    end
+
+    return self.new({
+      :mailing_list_address => mailing_list_address,
+      :name => member["name"],
+      :address => member["address"],
+      :subscribed => member["subscribed"]
+    })
+  end
+
+
+  private
+
+
+  def self.mailgun
+    Mailgun()
+  end
+
+  def mailgun
+    Mailgun()
+  end
+
+
+end
