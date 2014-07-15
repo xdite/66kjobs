d4083dd535caf35e424c3bad78e56b400d4660b9
xdite <xuite.joke@gmail.com>
2014-07-15 12:39:01 +0800

    also highligh yesterday's job add number to currency

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -20,18 +20,21 @@ module JobsHelper
 
   def render_job_salary(salary)
 
-    special_label = if salary > 200000
+    special_label = if salary >= 200000
       "label-danger"
     elsif salary >= 100000
       "label-success"
     else
       ""
     end
+
+    salary = number_to_currency(salary, precision: 0)
+
     content_tag(:span, "新台幣 $#{salary} 元", :class => "label label-default #{special_label}")
   end
 
   def render_job_fresh_state(job)
-    if job.created_on == Date.today
+    if job.created_on >= Date.today - 1.days
       content_tag(:span, "NEW", :class => "label label-success")
     end
 
