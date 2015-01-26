8d9c93cc51839522c1df1e0971e44dc7ce3a3bfd
xdite <xdite@spoonrocket.com>
2015-01-26 11:51:27 -0800

    use 2.1.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :runner, "apps"
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :use_sudo, false
-set :rvm_ruby_string, '2.0.0'
+set :rvm_ruby_string, '2.1.2'
 
 set :whenever_command, "bundle exec whenever"
 #set :hipchat_token, APP_CONFIG["production"]["hipchat_token"]
